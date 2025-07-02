c8267a2ac2d3619553b381599f6849c118534e71
Eliu2001 <135901929+Eliu2001@users.noreply.github.com>
2025-07-02 00:56:56 -0400

    animaciones

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ $(document).ready(function() {
     });
 
     // 2. Botón Proyectos animado (tamaño y color con jQuery)
-    $('#btn-project').on('mouseenter', function() {
+    $('#btn-project').on('mouseenter focus', function() {
         $(this).stop().animate({ fontSize: '2.5rem', paddingLeft: '2.5rem', paddingRight: '2.5rem' }, 180)
                .css({ backgroundColor: '#8A784E', color: '#E7EFC7' });
-    }).on('mouseleave', function() {
+    }).on('mouseleave blur', function() {
         $(this).stop().animate({ fontSize: '2rem', paddingLeft: '1.5rem', paddingRight: '1.5rem' }, 180)
                .css({ backgroundColor: '#AEC8A4', color: '#3B3B1A' });
     }).on('click', function(e) {
@@ -24,19 +24,12 @@ $(document).ready(function() {
         }, 1200);
     });
 
-    // 3. Animación en el carrusel al mostrar slide (usando 'shown.bs.carousel')
+    // 3. Animación en el carrusel al mostrar slide (usando 'slid.bs.carousel')
     $('#carouselExampleInterval').on('slid.bs.carousel', function (e) {
         var $active = $(this).find('.carousel-item.active');
-        $active.find('p, a').css({opacity:0, position:'relative', top:'30px'});
-        $active.find('p, a').each(function(i) {
+        $active.find('.carousel-hover-tab p, .carousel-hover-tab a').css({opacity:0, position:'relative', top:'30px'});
+        $active.find('.carousel-hover-tab p, .carousel-hover-tab a').each(function(i) {
             $(this).delay(200*i).animate({opacity:1, top:0}, 500);
         });
-    });
-
-    // Inicializa la animación en el primer slide
-    var $first = $('#carouselExampleInterval .carousel-item.active');
-    $first.find('p, a').css({opacity:0, position:'relative', top:'30px'});
-    $first.find('p, a').each(function(i) {
-        $(this).delay(200*i).animate({opacity:1, top:0}, 500);
     });
 });
