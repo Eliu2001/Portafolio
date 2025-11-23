6f1def5294fb8ef61fe4ac864b7568d827efe738
Eliu2001 <villalobosgermain2@gmail.com>
2025-11-23 18:23:17 -0300

    fix: update API endpoint to use relative path for contact form submission

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ $(document).ready(function() {
             }
 
             try {
-                const response = await fetch('http://localhost:3000/api/contact', {
+                const response = await fetch('/api/contact', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
